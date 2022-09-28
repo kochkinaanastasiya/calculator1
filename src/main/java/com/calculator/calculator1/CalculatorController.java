9db6425db710c2b6f61724abcb74594fe74e3ff4
Anastasiya Kochkina <kochkinaanastasiya@yandex.ru>
2022-09-28 18:03:52 +0300

    init

---
+++ src/main/java/com/calculator/calculator1/CalculatorController.java
@@ -0,0 +1,61 @@
+package com.calculator.calculator1;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    }
+    @GetMapping()
+    public String welcome(){
+        return calculatorService.welcome();
+    }
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2){
+        if(num1 == null || num2 == null) {
+            return "Не задан парметр";
+        }
+            String num3 = Integer.toString(num1);
+            String num4 = Integer.toString(num2);
+            int result = calculatorService.plus(num1, num2);
+            return num3 + " + " + num4 + " = " + result;
+        }
+
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2){
+        if(num1 == null || num2 == null) {
+            return "Не задан парметр";
+        }
+        String num3 = Integer.toString(num1);
+        String num4 = Integer.toString(num2);
+        int result = calculatorService.minus(num1, num2);
+        return num3 + " - " + num4 + " = " + result;
+    }
+    @GetMapping(path = "/multiply")
+    public String multiply(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2){
+        if(num1 == null || num2 == null) {
+            return "Не задан парметр";
+        }
+        String num3 = Integer.toString(num1);
+        String num4 = Integer.toString(num2);
+        int result = calculatorService.multiply(num1, num2);
+        return num3 + " * " + num4 + " = " + result;
+    }
+
+    @GetMapping(path = "/divide")
+    public String divide(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2){
+        if(num1 == null || num2 == null) {
+            return "Не задан парметр";
+        } else if(num2 == 0){
+            return "Ошибка, на ноль делить нельзя";
+        }
+        String num3 = Integer.toString(num1);
+        String num4 = Integer.toString(num2);
+        int result = calculatorService.divide(num1, num2);
+        return num3 + " / " + num4 + " = " + result;
+    }
+}
---
+++ src/main/java/com/calculator/calculator1/CalculatorService.java
@@ -0,0 +1,10 @@
+package com.calculator.calculator1;
+
+public interface CalculatorService {
+    String welcome();
+
+    int plus(int num1, int num2);
+    int minus(int num1, int num2);
+    int multiply(int num1, int num2);
+    int divide(int num1, int num2);
+}
